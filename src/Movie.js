b69d34e88d145a302d127e82258ef384b2b2a02f
Saerom <saerombang11@gmail.com>
2019-05-29 18:36:50 +0900

    first commit

---
+++ src/Movie.js
@@ -0,0 +1,8 @@
+import React from react;
+import Proptypes from Proptypes;
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+
+const POSTER_PATH
+
