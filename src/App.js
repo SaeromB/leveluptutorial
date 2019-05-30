9426fa09e8070abcb91d2d3d19064c914c7fbc78
Saerom <saerombang11@gmail.com>
2019-05-30 18:24:17 +0900

    added propTypes

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
+        {movies.map(movie=> <Movie key={movie.id} movie={movie} desc={movie.desc}/>)}
       </div>
     );
   }
