c65f7a9f7ea6c41b534ceecbe59f4ef34622f254
Saerom <saerombang11@gmail.com>
2019-05-31 15:57:43 +0900

    added a movie api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, {Component} from 'react';
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Movie from './Movie';
 
-const movies = [{
-  id: 1,
-  title: 'StarWars'
-},
-{
-  id: 2,
-  title: 'Spider Man'
-},
-{
-  id: 3,
-  title: 'Happy Deathday'
-},
-{
-  id: 4,
-  title: 'Ironman'
-}]
-
 class App extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=0423ed58e5855d234d54dd1e3efd26eb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie=> <Movie key={movie.id} movie={movie} desc={movie.desc}/>)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
