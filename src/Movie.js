d46bda66d64334f2978680ee7d63c32719cb8610
Saerom <saerombang11@gmail.com>
2019-06-05 17:55:55 +0900

    added router

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,18 @@
+//  `` JS variables into strings
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Movie = ({ movie }) => (
-  <div>
-    <h3>{movie.title}</h3>
-  </div>
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const Movie = ({movie}) => (
+<div>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>
+</div>
 );
 
 export default Movie;
-
 Movie.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-};
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    }).isRequired
+
+}
